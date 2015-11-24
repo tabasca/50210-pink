c926545eb75947e195144c4e4e4b7324f2c0e63c
Keks <keks@htmlacademy.ru>
2015-11-24 19:50:33 +0300

    :kissing_cat: Добавил сборку и ватч для LESS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
 "use strict";
 
 var gulp = require("gulp");
+var less = require("gulp-less");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+
+gulp.task("style", function() {
+  return gulp.src("less/style.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer({browsers: "last 2 versions"})
+    ]))
+    .pipe(gulp.dest("css"));
+});
+
+gulp.task("start", ["style"], function() {
+  gulp.watch("less/**/*.less", ["style"]);
+});
 
 
 
